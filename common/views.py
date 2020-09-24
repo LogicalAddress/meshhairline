8a78b0ceedde1c63f7f1947aaeeae0b280cd0dd9
Retnan <dretnan@logicaladdress.com>
2020-09-24 18:39:11 +0100

    m

--- common/views.py
+++ common/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from shop.cart.cart import Cart
 
 def switch_currency(request):
     if request.method == 'POST':
         pass
+    cart = Cart(request.session)
+    cart.clear()
     currency = request.GET['currency']
     request.session['currency'] = currency
     return redirect('/')
