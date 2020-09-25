bd1990d6ca1f76cd8e6a343ac1a63dba72992fc9
Retnan <dretnan@logicaladdress.com>
2020-09-25 12:31:47 +0100

    currency switcher in progress

--- shop/templatetags/shop_tags.py
+++ shop/templatetags/shop_tags.py
@@ -12,3 +12,19 @@ def get_cart(context):
     request = context['request']
     context['cart'] = Cart(request.session)
     return ''
+
+@register.simple_tag(takes_context=True, name='currency_price')
+def get_currency_price(context, price, currency):
+    if currency and currency == "USD":
+        rate = 360
+        price = float(price) / rate
+        return "%.2f" % round(price, 2)
+    else:
+        return float(price)
+
+@register.simple_tag(takes_context=True, name='currency_symbol')
+def get_currency_symbol(context, currency):
+    if currency and currency == 'USD':
+        return '$'
+    else:
+        return 'N'
